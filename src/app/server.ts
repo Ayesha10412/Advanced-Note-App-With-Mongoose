7139e4ef81966563ff11a1b0df8e174cc624148f
Your Name <k.bristi057@gmail.com>
2025-06-25 22:08:36 +0600

    Added the basic setup of mongoose

---
+++ src/app/server.ts
@@ -0,0 +1,18 @@
+import {Server} from 'http'
+import app from './app';
+import mongoose from 'mongoose';
+
+let server:Server ;
+const Port =5000;
+async function main(){
+    try{
+    await mongoose.connect('mongodb+srv://Practice:JbSutunp5HzJndRr@cluster0.i7pwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    console.log("Connecting to mongodb using mongoose.")
+server =app.listen(Port,()=>{
+    console.log(`App is listening on port6 ${Port}`)
+})
+    }catch(error){
+        console.log(error);
+    }
+}
+main()
