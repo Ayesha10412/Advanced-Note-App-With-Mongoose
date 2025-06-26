9c5ea4e6b8c37e5d5aaf194dcce4efccf1f30c12
Your Name <k.bristi057@gmail.com>
2025-06-26 17:58:34 +0600

    added the other crud operations and develop  MVC concept

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,102 +1,10 @@
 import express, { Application, Request, Response } from "express";
-import mongoose, { Schema } from "mongoose";
+import { noteRoutes } from "./controller/note.controller";
 const app:Application=express();
 app.use(express.json())
-const noteSchema=new Schema({
-    title:String,
-    content:String
-})
-
-const Note = mongoose.model('Note',noteSchema);
-app.post('/create-note',async(req:Request,res:Response)=>{
-    const myNote = new Note({
-        title:"Learning Mongoose!",
-        content:"I am learning Mongoose."
-    })
-    await myNote.save()
-    res.status(201).json({success:true,message:"Note created successfully!",note:myNote})
-})
-
-const userSchema=new Schema({
-    name:String,
-    email:String,
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
-})
-const userNote = mongoose.model("userNote",userSchema)
-app.post("/create-userNote",async(req:Request,res:Response)=>{
-    const user=new userNote({
-        name:"Ayesha",
-        email:"ayesha@gmail.com"
-    })
-    await user.save()
-    res.status(201).json(
-        {
-            success:true,
-            message:"User note created successfully!",
-            note:user
-        }
-    )
-})
-//enum is a field of predefined list of options
-const appSchema=new Schema({
-title:{type:String,required:true,trim:true},
-content:{type:String,default:''},
-category:{
-    type:String,
-    enum:["personal","work","study","other"],
-    default:"personal"
-},
-pinned:{
-    type:Boolean,
-    default:false
-},
-tags:{
-    label:{type:String,required:true},
-    color:{type:String,default:'Green'}
-}
-
-})
-const appNoteSchema=mongoose.model("appNoteSchema",appSchema)
-app.post('/create-advanced-note',async(req:Request,res:Response)=>{
-    //approach1
-    const advancedNote = new appNoteSchema({
-        title:"Learning Node",
-        tags:{
-            label:'database'
-        }
-    })
-    await advancedNote.save()
-    res.status(201).json({
-        success:true,message:"Advanced note created successfully.",note:advancedNote
-    })
-})
-app.post('/note/create-advanced-note',async(req:Request,res:Response)=>{
-    const body=req.body;
-    const note = await appNoteSchema.create(body)
-    res.status(201).json({
-        success:true,message:"Advanced note created successfully.",note:note
-    })
-})
-app.get('/notes',async(req:Request,res:Response)=>{
-    const notes = await appNoteSchema.find();
- res.status(201).json({
-        success:true,message:"Advanced note created successfully.",note:notes
-    })})
-
-app.get('/notes/:noteId',async(req:Request,res:Response)=>{
-    const noteId = req.params.noteId
-    // const note= await appNoteSchema.findById(noteId)
-    const note =await appNoteSchema.findOne({_id:noteId})
-     res.status(201).json({
-        success:true,message:"Advanced note created successfully.",note
-    })
-
-})    
-
+app.use('/notes',noteRoutes)
 app.get('/',(req:Request,res:Response)=>{
     res.send('Welcome to Note App!')
 })
 export default app;
+//MVC-- Model, View, Controller
---
+++ src/app/controller/note.controller.ts
@@ -0,0 +1,81 @@
+import express,{ Request, Response } from "express"
+import { appNoteSchema, Note, userNote } from "../models/note.models"
+export const noteRoutes = express.Router()
+noteRoutes.post('/create-note',async(req:Request,res:Response)=>{
+    const myNote = new Note({
+        title:"Learning Mongoose!",
+        content:"I am learning Mongoose."
+    })
+    await myNote.save()
+    res.status(201).json({success:true,message:"Note created successfully!",note:myNote})
+})
+
+noteRoutes.post("/create-userNote",async(req:Request,res:Response)=>{
+    const user=new userNote({
+        name:"Ayesha",
+        email:"ayesha@gmail.com"
+    })
+    await user.save()
+    res.status(201).json(
+        {
+            success:true,
+            message:"User note created successfully!",
+            note:user
+        }
+    )
+})
+//enum is a field of predefined list of options
+noteRoutes.post('/create-advanced-note',async(req:Request,res:Response)=>{
+    //approach1
+    const advancedNote = new appNoteSchema({
+        title:"Learning Node",
+        tags:{
+            label:'database'
+        }
+    })
+    await advancedNote.save()
+    res.status(201).json({
+        success:true,message:"Advanced note created successfully.",note:advancedNote
+    })
+})
+noteRoutes.post('/create-advanced-note',async(req:Request,res:Response)=>{
+    const body=req.body;
+    const note = await appNoteSchema.create(body)
+    res.status(201).json({
+        success:true,message:"Advanced note created successfully.",note:note
+    })
+})
+noteRoutes.get('/',async(req:Request,res:Response)=>{
+    const notes = await appNoteSchema.find();
+ res.status(201).json({
+        success:true,message:"Advanced note created successfully.",note:notes
+    })})
+
+noteRoutes.get('/:noteId',async(req:Request,res:Response)=>{
+    const noteId = req.params.noteId
+    // const note= await appNoteSchema.findById(noteId)
+    const note =await appNoteSchema.findOne({_id:noteId})
+     res.status(201).json({
+        success:true,message:"Advanced note created successfully.",note
+    })
+
+})    
+
+noteRoutes.patch('/:noteId',async(req:Request,res:Response)=>{
+    const noteId = req.params.noteId
+    const updatedBody = req.body
+    const note = await appNoteSchema.findByIdAndUpdate(noteId,updatedBody,{new:true})
+       res.status(201).json({
+        success:true,message:"Advanced note updated successfully.",note
+    })
+})
+
+noteRoutes.delete('/:noteId',async(req:Request,res:Response)=>{
+    const noteId = req.params.noteId
+    const note = await appNoteSchema.findByIdAndDelete(noteId);
+    // const note1= await appNoteSchema.deleteOne({_id:noteId})
+       // const note1= await appNoteSchema.findAndDelete({_id:noteId})
+       res.status(201).json({
+        success:true,message:"Advanced note updated successfully.",note
+    })
+})
